b191b0fc1b123bcd06c399efcd276d0cfdddbb0d
Rishikesh Maurya <rishikesm610@gmail.com>
2026-07-17 00:10:10 +0530

    Update CORS origin URL in index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://buy-ans-selling-the-rooms-2.onrender.com",
+    origin:"https://buy-ans-selling-the-rooms.onrender.com",
     credentials:true
 }))
 
